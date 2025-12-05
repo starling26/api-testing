ed941961b788473922e1e44491eb471ee0684a61
starling <starling0626@gmail.com>
2025-12-04 23:38:58 -0400

    New changes

--- pages/dummyjson/auth.page.ts
+++ pages/dummyjson/auth.page.ts
@@ -11,15 +11,37 @@ export class AuthPage {
       data: credentials
     });
   }
+  
+   nullLogin(credentials: { username: null; password: null }) {
+    return  this.request.post('/auth/login', {
+      data: credentials
+    });
+  }
 
    loginWithValidCredentials(credentials: { username: string; password: string }) {
     return  this.login(credentials);
+  }
+
+  accessProtectedEndpointWithInvalidToken(invalidToken: string) {
+    return  this.request.get('/auth/me', {
+      headers: {
+        Authorization: `Bearer ${invalidToken}`
+      }
+    });
   }
 
    refreshToken(refreshToken: string) {
     return  this.request.post('/auth/refresh', {
       data: {
         refreshToken
+      }
+    });
+  }
+
+  refreshTokenWithInvalidToken(invalidToken: string) {
+    return  this.request.post('/auth/refresh', {
+      headers: {
+        Authorization: `Bearer ${invalidToken}`
       }
     });
   }
